cd265b8488c3726c5990964b05d122c0de394d83
jamshid <jamshidazizov@gmail.com>
2020-02-21 19:22:58 +0200

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => { 
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_HcmugIPIUtSRflMk9XKKDigr00EJbvcXsd';
+
+    const onToken = token  => {
+        console.log(token);
+        alert('Payment successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Evoli'
+            billingAddress
+            shippingAddress
+            image=''
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
